07c1a51372190886d47f2766c33a42c511c0c4b8
Bruno Sonnino <sonnino@revolution.com.br>
2019-01-07 09:17:52 -0200

    Add project files.

---
+++ XamlEditAndContinue/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace XamlEditAndContinue
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ViewModel = new MainViewModel();
+            DataContext = ViewModel;
+        }
+
+        public MainViewModel ViewModel { get; set; }
+
+        private void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            myStoryboard.Begin();
+        }
+
+        private void SplitViewButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XamlEditAndContinue/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XamlEditAndContinue
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _textLabel;
+        private string _textLabel2;
+
+        public MainViewModel()
+        {
+            TextLabel = "Text Label";
+            TextLabel2 = "Text Label 2";
+        }
+        public string TextLabel
+        {
+            get => _textLabel;
+            set
+            {
+                _textLabel = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string TextLabel2
+        {
+            get => _textLabel2;
+            set
+            {
+                _textLabel2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
